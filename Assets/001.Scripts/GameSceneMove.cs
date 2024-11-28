107d0660536c25ce21043eba7f6d43d6eb544aa8
Chan <137601394+zjoqa@users.noreply.github.com>
2024-11-29 01:53:48 +0900

    update

---
+++ Assets/001.Scripts/GameSceneMove.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneMove : MonoBehaviour
+{
+    public void GameScnesCtrl()
+    {
+        SceneManager.LoadScene("GameScene_1");
+    }
+}
---
+++ Assets/001.Scripts/UI/Inventory/InventoryUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+    bool activeInventory = false;
+
+    void Start()
+    {
+        inventoryPanel.SetActive(activeInventory);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            activeInventory = !activeInventory;
+            inventoryPanel.SetActive(activeInventory);
+        }
+    }
+}
